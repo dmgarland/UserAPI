c52d638117137cd9854237b5c2639cdab7f85b47
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-07 13:57:19 +0100

    added render_views method to controller spec so now returning json from api

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  render_views
 
   describe "GET /users" do
     before do
@@ -15,7 +16,7 @@ RSpec.describe UsersController, type: :controller do
 
     it "returns Json" do 
       expect(response.content_type).to eq('application/json')
-      expect{ JSON(response.body) }.not_to raise_exception
+      expect{ JSON.parse(response.body) }.not_to raise_exception
       user_json = JSON(response.body)[0]
       expect(user_json["first_name"]).to eq "Dan"
       expect(user_json["last_name"]).to eq "Garland"
@@ -55,9 +56,28 @@ RSpec.describe UsersController, type: :controller do
     it "returns JSON" do 
       expect(response.content_type).to eq('application/json')
       expect{ JSON(response.body) }.not_to raise_exception
-      # user_json = JSON(response.body)
-      # expect(user_json["first_name"]).to eq "Dan"
-      # expect(user_json["last_name"]).to eq "Garland"
+      user_json = JSON(response.body)
+      expect(user_json["first_name"]).to eq "Sophie"
+      expect(user_json["last_name"]).to eq "Figgis"
+    end
+  end
+
+  describe "DELETE /users" do
+    before do
+      @user = User.create!(first_name: "Dan", last_name: "Garland")
+      destroy :delete, params: {:id => @user.id}, :format => :json
+    end
+
+    it "returns 200" do
+      expect(response.status).to eq(200)
+    end
+
+    it "returns JSON" do 
+      expect(response.content_type).to eq('application/json')
+      expect{ JSON(response.body) }.not_to raise_exception
+      user_json = JSON(response.body)
+      expect(user_json["first_name"]).to eq "Sophie"
+      expect(user_json["last_name"]).to eq "Figgis"
     end
   end
 
