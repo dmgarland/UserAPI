cc95ccc03b4f44203e230ea6942d716248e5fe7c
Sophie Figgis <sophiefiggis@gmail.com>
2017-08-08 13:55:51 +0100

    added test for update method in controller. JSON coming in correctly"

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -38,9 +38,30 @@ RSpec.describe ProjectsController, type: :controller do
     it "returns JSON" do 
       expect(response.content_type).to eq('application/json')
       expect{ JSON(response.body) }.not_to raise_exception
-      user_json = JSON(response.body)
-      expect(user_json["name"]).to eq "Some project"
-      expect(user_json["description"]).to eq "saying some stuff about project"
+      project_json = JSON(response.body)
+      expect(project_json["name"]).to eq "Some project"
+      expect(project_json["description"]).to eq "saying some stuff about project"
+    end
+  end
+
+  describe "UPDATE /projects" do
+    before do
+      @user = User.create!(first_name: "Sophie", last_name: "Figgis")
+      @project = @user.projects.create!(name: "Gravy Train", description: "saying some stuff about project")
+      @updated = {name: "Apples and pears", description: "Goodness gracious me!!"}
+      put :update, params: {:id => @project.id, :project => @updated}, :format => :json
+    end
+
+    it "returns 200" do
+      expect(response.status).to eq(200)
+    end
+
+    it "returns JSON" do 
+      expect(response.content_type).to eq('application/json')
+      expect{ JSON(response.body) }.not_to raise_exception
+      project_json = JSON(response.body)
+      expect(project_json["name"]).to eq "Apples and pears"
+      expect(project_json["description"]).to eq "Goodness gracious me!!"
     end
   end
 
